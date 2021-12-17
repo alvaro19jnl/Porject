e8ee539d0affe422cca6fdfa85613d34d5d1cbeb
alvaro19jnl <80657010+alvaro19jnl@users.noreply.github.com>
2021-12-17 09:34:17 -0500

    Delete storage directory

--- storage/framework/views/28dd07484c33b4af6dcdd2afa0ae3675cd46b69b.php
+++
@@ -1,6 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-
-<h1>Mision</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layaut', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\example-app\resources\views/mision.blade.php ENDPATH**/ ?>
