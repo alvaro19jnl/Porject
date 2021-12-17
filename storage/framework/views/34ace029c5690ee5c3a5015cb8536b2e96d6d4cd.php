4b39a2335f5f48f6ffc5a8a0c991ef880321299e
alvaro19jnl <80657010+alvaro19jnl@users.noreply.github.com>
2021-12-17 09:32:02 -0500

    Add files via upload

---
+++ storage/framework/views/34ace029c5690ee5c3a5015cb8536b2e96d6d4cd.php
@@ -0,0 +1,16 @@
+
+<?php $__env->startSection('pageCurrent'); ?>
+
+    <div>
+        <form enctype="multipart/form-data" id="frmUploadImages" action="/fileUpload" method="post" >
+            <input type="text" name="nombreImage">
+            <br>
+            <input type="file">
+            <?php echo csrf_field(); ?>
+            <br>
+            <input type="submit" value="Enviar" >
+        </form>
+    </div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('plataform', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\example-app\resources\views/upimage.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e6ea29f231b6d731ebed74757b08327b8970427e.php
@@ -0,0 +1,22 @@
+
+<?php $__env->startSection('content'); ?>
+<LINK REL=StyleSheet HREF="style3.css" TYPE="text/css" MEDIA=screen>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario</title>
+    <LINK REL=StyleSheet HREF="style3.css" TYPE="text/css" MEDIA=screen>
+
+</head>
+<body>
+<section>
+
+</section>
+</body>
+
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layaut', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\User\example-app\resources\views/login.blade.php ENDPATH**/ ?>
